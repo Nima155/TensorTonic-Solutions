fb632c937181906830ed1743d827e452db407723
Nima155 <59487650+Nima155@users.noreply.github.com>
2026-05-14 21:34:14 +0200

    Update solution for auc

--- auc/auc.py
+++ auc/auc.py
@@ -7,7 +7,4 @@ def auc(fpr, tpr):
     # Write code here
     fpr = np.array(fpr)
     tpr = np.array(tpr)
-    # print(np.pad(tpr[1:], 1))
-    # print(tpr[:] + np.pad(tpr[1:], 1)[1:])
-    # print(tpr[:] + np.pad(tpr[1:], 1)[1:])
     return (((1/2) * ((tpr[:] + np.pad(tpr[1:], 1)[1:]))[:-1]) * ((np.pad(fpr[1:], 1)[1:] - fpr))[:-1]).sum()
