8ca495985b3547946f33255895be3964395542af
Nima155 <59487650+Nima155@users.noreply.github.com>
2026-05-24 11:12:33 +0200

    Update solution for vgg-conv-block

--- vgg/vgg-conv-block/vgg-conv-block.py
+++ vgg/vgg-conv-block/vgg-conv-block.py
@@ -4,12 +4,7 @@ def vgg_conv_block(x: np.ndarray, weights: list, biases: list) -> np.ndarray:
     """
     Returns: np.ndarray of shape (B, H, W, C_out) after sequential linear transforms with ReLU
     """
-    # Your implementation here
-    # print(x.shape, np.array(weights).shape)
-    # out = x @ np.array(weights) + np.array(biases)
-    # out[out < 0] = 0
     for w, b in zip(weights, biases):
         x = x @ np.array(w) + np.array(b)
         x[x < 0] = 0
     return x
-    # return out
