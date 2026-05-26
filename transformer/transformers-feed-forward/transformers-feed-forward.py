e0e99b2d28499079ba61a5dd71e072c1b87732c7
Nima155 <59487650+Nima155@users.noreply.github.com>
2026-05-26 11:38:51 +0200

    Add solution for transformers-feed-forward

---
+++ transformer/transformers-feed-forward/transformers-feed-forward.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def feed_forward(x: np.ndarray, W1: np.ndarray, b1: np.ndarray,
+                 W2: np.ndarray, b2: np.ndarray) -> np.ndarray:
+    """
+    Apply position-wise feed-forward network.
+    """
+    # Your code here
+    r = x @ W1 + b1
+    r[r < 0] = 0
+    return r @ W2 + b2
