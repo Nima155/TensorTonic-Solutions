950d8644ea29b1d2ad25db54d594fc4816d18dfc
Nima155 <59487650+Nima155@users.noreply.github.com>
2026-06-01 23:38:09 +0200

    Update solution for ml-logistic-regression

--- study-plans/cracking-ml/ml-logistic-regression/ml-logistic-regression.py
+++ study-plans/cracking-ml/ml-logistic-regression/ml-logistic-regression.py
@@ -11,12 +11,8 @@ def logistic_regression(X, y, lr=0.01, n_iters=1000):
     w = np.zeros(X.shape[1])
     N = X.shape[0]
     for _ in range(n_iters):
-        # print(X.shape, w.shape, b.shape)
         z = (X @ w) + b
         sigz = 1 / (1 + np.exp(-z))
-        # mask = sigz >= 0.5
-        # # sigz[mask] = 1
-        # # sigz[~mask] = 0
         w -= lr * (1/N) * X.T @ (sigz - y)
         b -= lr * (1/N) * ((sigz - y).sum())
     return (w, b[0])
